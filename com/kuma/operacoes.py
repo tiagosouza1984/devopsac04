d14f96a2e2eee5a3d0808268cf4c0bfc1a1eb2ac
Tiago da Silva Souza <1900629@SALAS.AULAS>
2019-09-13 21:10:50 -0300

    adicionando diretorios ao github no repositorio devopsac04

---
+++ com/kuma/operacoes.py
@@ -0,0 +1,15 @@
+class Operacoes():
+
+    def soma(self, valores):
+        val = 0
+        for v in valores:
+            val = val + v
+
+        return val
+
+    def subtracao(self, valores):
+        val = 0
+        for v in valores:
+            val = val - v
+        return val
+
